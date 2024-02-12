0bba8703f1364edca5fa1203c47dd13139f2dcd3
Dima Bely <deem4an@gmail.com>
2024-02-12 20:41:58 +0300

    gg

---
+++ pythonCourse/pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+categories = data['whoAmI'].unique()
+for category in categories:
+    data[category] = (data['whoAmI'] == category).astype(int)
+data.drop('whoAmI', axis=1, inplace=True)
+data.head()
